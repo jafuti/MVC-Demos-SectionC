634d5631c90f09b05768fa276fd49a8b6ddf39dd
jafare2010@gmail.com <jafare2010@gmail.com>
2021-11-02 14:05:59 +0300

    first commit

---
+++ MVCDEMOSECTIONC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDEMOSECTIONC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View("Contact");
+        }
+        public ActionResult GetStudentName(int stdId)
+        {
+            var students = new[]
+            {
+              new {stdId=101, Name="Abebe"},
+              new {stdId=102, Name="Kebede"},
+          };
+            string studentName = null;
+            foreach(var item in students)
+            {
+                if (item.stdId == stdId)
+                {
+                    studentName = item.Name;
+                }
+            }
+            return Content(studentName, "text/plain");
+        }
+    }
+}
---
+++ MVCDEMOSECTIONC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCDEMOSECTIONC.MvcApplication" Language="C#" %>
